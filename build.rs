e4c42330ac58f34078cdd511d64e0186ea871bc9
GToidZ <39559733+GToidZ@users.noreply.github.com>
2025-02-15 15:51:56 +0700

    feat: add sample usage to main source file showing how to use rpc

--- build.rs
+++ build.rs
@@ -4,6 +4,11 @@ use std::env;
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
+        .compile_well_known_types(true)
         .compile_protos(&["protocol/domain.proto"], &["protocol"])
+        .unwrap();
+    tonic_build::configure()
+        .build_client(false)
+        .compile_protos(&["protocol/data/main.proto"], &["protocol"])
         .unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,65 @@
+use kueater::{LocalizedString, Ingredient, MenuItem};
+use kueater::data::{GetMenuRequest, GetMenuResponse};
+use kueater::data::ku_eater_backend_server::{KuEaterBackend, KuEaterBackendServer};
+use tonic::transport::Server;
+use tonic::{Status, Request, Response};
+
 pub mod kueater {
     tonic::include_proto!("kueater");
+    pub mod data {
+        tonic::include_proto!("kueater.data");
+        pub mod index {
+            tonic::include_proto!("kueater.data.index");
+        }
+        pub mod search {
+            tonic::include_proto!("kueater.data.search");
+        }
+    }
 }
 
-fn main() {
+#[derive(Debug, Default)]
+pub struct BackendService {}
 
+#[tonic::async_trait]
+impl KuEaterBackend for BackendService {
+
+    async fn get_menu_item(
+        &self,
+        request: Request<GetMenuRequest>,
+    ) -> Result<Response<GetMenuResponse>, Status> {
+        let resp = GetMenuResponse {
+            item: Some(MenuItem {
+                uuid: request.into_inner().uuid,
+                name: Some(
+                    LocalizedString { content: "Chicken Wings".to_string() , locale: "en".to_string() }
+                ),
+                price: 30.0,
+                ingredients: vec![
+                    Ingredient {
+                        uuid: "2".to_string(),
+                        name: Some(
+                            LocalizedString { content: "Chicken".to_string() , locale: "en".to_string() }
+                        )
+                    }
+                ],
+                image: "".to_string(),
+                tags: vec![]
+            })
+        };
+        Ok(Response::new(resp))
+    }
+
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let service = BackendService::default();
+
+    Server::builder()
+        .add_service(KuEaterBackendServer::new(service))
+        .serve(addr)
+        .await?;
+    
+    Ok(())
 }
